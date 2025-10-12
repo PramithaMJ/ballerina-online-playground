a6325f30ad60f947f5d1322afeee3b35ce9c866e
Pramitha Jayasooriya <lpramithamj@gmail.com>
2025-10-12 22:26:06 +0530

    debug: Add logging to trace CORS headers and response encoding

--- backend/handler/run.go
+++ backend/handler/run.go
@@ -28,7 +28,9 @@ func RunCode(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, HEAD")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
 	w.Header().Set("Access-Control-Expose-Headers", "Content-Length, Content-Type")
-	
+
+	log.Printf("DEBUG: Set CORS headers with origin: %s", origin)
+
 	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
@@ -107,5 +109,12 @@ func RunCode(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+
+	// Log response headers before sending
+	log.Printf("DEBUG: Response headers: %v", w.Header())
+	log.Printf("DEBUG: Sending response with output length: %d", len(response.Output))
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		log.Printf("ERROR: Failed to encode response: %v", err)
+	}
 }
